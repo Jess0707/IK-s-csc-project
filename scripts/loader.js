03fc319f798e769eaef6e1790f0312d71f67b804
Udensi Ikechukwu <124217589+Jess0707@users.noreply.github.com>
2025-12-29 19:37:25 +0100

    fix

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     if (loader) {
-        // Minimal delay to ensure the loader is seen briefly
         setTimeout(() => {
             loader.classList.add('is-hidden');
         }, 800);
